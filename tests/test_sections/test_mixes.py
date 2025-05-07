d325d0420b63d4fe9e6b251cc0cf99f36c2f46a7
Nathan Menge <nathan.menge@gmail.com>
2025-05-07 19:24:46 -0400

    [Tests] Catching up missed test changes

--- tests/test_sections/test_mixes.py
+++ tests/test_sections/test_mixes.py
@@ -8,7 +8,7 @@ def test_act(connection_thread_mock, session_data):
 def test_build(client_mock, connection_thread_mock, session_data):
     observed = mixes.build()
     expected = {
-        "body": '<div class="tab-content not-prose justify-center flex flex-col m-auto" hx-get="/ui/mixes" hx-trigger="load" hx-swap="innerHTML"></div>',
+        "body": '<div class="tab-content not-prose justify-center flex flex-col m-auto w-full" hx-get="/ui/mixes" hx-trigger="load" hx-swap="innerHTML"></div>',
         "cookies": [],
         "headers": {"Content-Type": "text/html"},
         "isBase64Encoded": False,
